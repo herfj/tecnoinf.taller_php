90f4a7ea699b2199656000e37f2999b423315c6f
Herfj <54822117+Herfj@users.noreply.github.com>
2021-05-08 15:26:01 -0300

    Migrations Tabla Users y Intitutos

--- database/migrations/2021_05_08_174928_update_users_table.php
+++ database/migrations/2021_05_08_174928_update_users_table.php
@@ -14,8 +14,8 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nickname')->unique();
-            $table->dateTime('birthday_date')->nullable();
+            $table->string('nickname')->unique()->after('name');
+            $table->dateTime('birthday_date')->nullable()->after('nickname');
         });
     }
 
@@ -26,6 +26,10 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('nickname');
+            $table->dropColumn('birthday_date');
+        });
+
     }
 }
