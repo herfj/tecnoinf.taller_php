a4f359e027efff23e1b9a44a9fdc3a89c8815260
jacomo <jackfishipa@gmail.com>
2021-05-11 22:31:10 -0300

    migratchiones

---
+++ database/migrations/2021_05_12_012508_create_enrollment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrollmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->integer('edition_id');
+            $table->string('state');
+            $table->text('description');
+            $table->float('course_grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollments');
+    }
+}
