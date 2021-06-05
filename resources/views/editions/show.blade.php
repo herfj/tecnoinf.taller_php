4f10a1f0e1b5a474d92b2c102bcb1aea7f4e1559
Herfj <54822117+Herfj@users.noreply.github.com>
2021-06-05 18:02:43 -0300

    ver inscripcion boton

--- resources/views/editions/show.blade.php
+++ resources/views/editions/show.blade.php
@@ -2,37 +2,43 @@
 @section('title',  $edition->name . ' - Curso')
 @section('content')
     <div style="display:flex">
-        <div class="h-100 p-5 border bg-light rounded-3 mt-5 mb-5" style="width:70%;margin-right:1.5%;word-break: break-all;">
+        <div class="h-100 p-5 border bg-light rounded-3 mt-5 mb-5"
+             style="width:70%;margin-right:1.5%;word-break: break-all;">
             <h1>{{$edition->name}}</h1>
-            <p class="card-text"> <strong>Fecha de Inicio: </strong>
+            <p class="card-text"><strong>Fecha de Inicio: </strong>
                 {{$edition->start_at}}
             </p>
-            <p class="card-text"> <strong>Fecha final: </strong>
+            <p class="card-text"><strong>Fecha final: </strong>
                 {{$edition->end_at}}
             </p>
-            <p class="card-text"> <strong>Cupos: </strong>
+            <p class="card-text"><strong>Cupos: </strong>
                 {{$edition->space_available}}
             </p>
 
-            <p class="card-text"> <strong>Profesor: </strong>
-            @foreach($teacher as $profe)
-                @if($profe->id==$edition->teacher_id)
-                    {{$profe->name}}
-                @endif
-            @endforeach
+            <p class="card-text"><strong>Profesor: </strong>
+                @foreach($teacher as $profe)
+                    @if($profe->id==$edition->teacher_id)
+                        {{$profe->name}}
+                    @endif
+                @endforeach
             </p>
-            <a href="{{route('courses.show',$edition->course_id)}}" class="btn btn-outline-secondary btn-sm ">Volver al curso</a>
-
-
-                @if(Auth::check() && ($edition->space_available > 0) && (Auth::user()->type_of_user==="student") && (Carbon\Carbon::now()->toDateTimeString() < Carbon\Carbon::createFromFormat('Y-m-d',$edition->start_at)))
-                    <form action="{{route('enrollments.create',$edition)}}" method="GET" >
-                        @csrf
-                        <button type="submit" class="btn btn-outline-primary btn-sm ml-5 mt-3"> Solicitar inscripcion</button>
-                    </form>
+            <a href="{{route('courses.show',$edition->course_id)}}" class="btn btn-outline-secondary btn-sm ">Volver al
+                curso</a>
+            @if(Auth::check() && ($edition->space_available > 0) && (Auth::user()->type_of_user==="student") && (Carbon\Carbon::now()->toDateTimeString() < Carbon\Carbon::createFromFormat('Y-m-d',$edition->start_at)))
+                @if($user_has_enroll)
+                    <a href="{{route('enrollments.en_state',$enroll)}}" class="btn btn-outline-primary btn-sm ">Ver inscripción</a>
+                @else
+                <form action="{{route('enrollments.create',$edition)}}" method="GET">
+                    @csrf
+                    <button type="submit" class="btn btn-outline-primary btn-sm ml-5 mt-3"> Solicitar inscripcion
+                    </button>
+                </form>
                 @endif
+            @endif
             @if(Auth::check() && ((Auth::user()->type_of_user==="teacher" && Auth::user()->id==$edition->teacher_id) || Auth::user()->type_of_user==="admin") )
-                <a href="{{route('editions.edit',$edition)}}" class="btn btn-outline-primary btn-sm ml-5">Editar Edicion</a>
-                <form action="{{route('editions.destroy',$edition)}}" method="POST" >
+                <a href="{{route('editions.edit',$edition)}}" class="btn btn-outline-primary btn-sm ml-5">Editar
+                    Edicion</a>
+                <form action="{{route('editions.destroy',$edition)}}" method="POST">
                     @csrf
                     @method('delete')
                     <button type="submit" class="btn btn-outline-danger btn-sm ml-5 mt-3"> Eliminar Edicion</button>
@@ -41,17 +47,20 @@
         </div>
         <div class="h-100 p-5 border bg-light rounded-3 mt-5 mb-5" style="width:30%; word-break: break-all">
             <ul class="list-group list-group-flush">
-                <li class="list-group-item" style="background-color: transparent; text-align: center;" ><h4>Clases</h4></li>
+                <li class="list-group-item" style="background-color: transparent; text-align: center;"><h4>Clases</h4>
+                </li>
                 @foreach($clases as $cl)
                     @if($cl->edition_id==$edition->id)
-                        <li class="list-group-item" style="background-color: transparent; text-align: center;" ><a href="{{route('classes.show',$cl->id)}}">{{$cl->topic}}</a></li>
+                        <li class="list-group-item" style="background-color: transparent; text-align: center;"><a
+                                href="{{route('classes.show',$cl->id)}}">{{$cl->topic}}</a></li>
                     @endif
                 @endforeach
-                <li class="list-group-item" style="background-color: transparent; text-align: center;" >
+                <li class="list-group-item" style="background-color: transparent; text-align: center;">
                     @if(Auth::check() && ((Auth::user()->type_of_user==="teacher" && Auth::user()->id==$edition->teacher_id) || Auth::user()->type_of_user==="admin") )
-                        <form action="{{route('classes.create',$edition->id)}}" method="POST" >
+                        <form action="{{route('classes.create',$edition->id)}}" method="POST">
                             @csrf
-                            <button type="submit" class="btn btn-outline-primary btn-sm ml-5 mt-3"> Crear una clase</button>
+                            <button type="submit" class="btn btn-outline-primary btn-sm ml-5 mt-3"> Crear una clase
+                            </button>
                     @endif
                 </li>
             </ul>
@@ -59,7 +68,7 @@
         <div>
             @if(Auth::check() && ((Auth::user()->type_of_user==="teacher" && Auth::user()->id==$edition->teacher_id) || Auth::user()->type_of_user==="admin") )
                 @foreach($enrollments as $enro)
-                    <form action="{{route('enrollments.accept')}}" method="POST" >
+                    <form action="{{route('enrollments.accept')}}" method="POST">
                     </form>
                 @endforeach
             @endif
